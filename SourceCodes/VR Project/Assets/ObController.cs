0b98834cb072b5da5dab0847982b5760623e0565
Ridhi Puppala <puppala.ridhi@gmail.com>
2019-10-16 02:42:39 +0530

    CNN Source Codes
    CNN source codes

---
+++ SourceCodes/VR Project/Assets/ObController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class ObController : MonoBehaviour
+{
+    #region private members 	
+    	
+    /// TCPListener to listen for incomming TCP connection requests. 	
+    	
+    private TcpListener tcpListener;
+    /// Background thread for TcpServer workload. 	
+    private Thread tcpListenerThread;
+    /// Create handle to connected tcp client. 	
+    private TcpClient connectedTcpClient;
+    #endregion
+    public string clientMessage;
+    public GameObject Cam;
+    private float time = 0.0f;
+    public float interpolationPeriod = 0.01f;
+    
+    // Use this for initialization
+    void Start()
+    {
+        // Start TcpServer background thread 		
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= interpolationPeriod)
+        {
+            time = time - interpolationPeriod;
+        }
+        string[] data = clientMessage.Split(',');
+        Vector3 pose = new Vector3(Single.Parse(data[0]), Single.Parse(data[1]), Single.Parse(data[2]));
+        Cam.transform.position = pose;
+        Cam.transform.rotation = Quaternion.Euler(0.0f, 45.0f, 0.0f);
+        
+    }
+
+    /// <summary> 	
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
+    /// </summary> 	
+    private void ListenForIncommingRequests()
+    {
+        try
+        {
+            // Create listener on localhost port 8052. 			
+            tcpListener = new TcpListener(IPAddress.Parse("192.168.1.109" + ""), 52595);
+            tcpListener.Start();
+            Debug.Log("Server is listening");
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    // Get a stream object for reading 					
+                    using (NetworkStream stream = connectedTcpClient.GetStream())
+                    {
+                        int length;
+                        // Read incomming stream into byte arrary. 						
+                        while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            // Convert byte array to string message. 							
+                            clientMessage = Encoding.ASCII.GetString(incommingData);
+                            Debug.Log("client message received as: " + clientMessage);
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
+    }
+    /// <summary> 	
+    /// Send message to client using socket connection. 	
+    /// </summary> 	
+    private void SendMessage()
+    {
+        if (connectedTcpClient == null)
+        {
+            return;
+        }
+
+        try
+        {
+            // Get a stream object for writing. 			
+            NetworkStream stream = connectedTcpClient.GetStream();
+            if (stream.CanWrite)
+            {
+                string serverMessage = "This is a message from your server.";
+                // Convert string message to byte array.                 
+                byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                // Write byte array to socketConnection stream.               
+                stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+                Debug.Log("Server sent his message - should be received by client");
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+}
---
+++ SourceCodes/VR Project/Assets/ObjectController.cs
@@ -0,0 +1,145 @@
+﻿
+        using UnityEngine;
+        using System.Collections;
+        using System;
+        using System.IO;
+        using System.Net.Sockets;
+        using System.Threading;
+
+
+
+public class ObjectController : MonoBehaviour
+    {
+        public String host = "192.168.1.102";
+        public Int32 port = 65432;
+
+        internal Boolean socket_ready = false;
+        internal String input_buffer = "";
+
+        TcpClient tcp_socket;
+        NetworkStream net_stream;
+
+        StreamWriter socket_writer;
+        StreamReader socket_reader;
+
+        public string received_data;
+        public GameObject Cam;
+        //Vector3 pose = new Vector3(0.0f, 0.0f, 0.0f);
+        public string C;
+
+        private void Start()
+        {
+            setupSocket();
+            //Console.WriteLine("Socket is set up");
+            //
+        }
+
+
+    void Update()
+    {
+        Debug.Log("Hello");
+        received_data = readSocket();
+        C = Decode(received_data);
+        Debug.Log(C);
+        
+        //Console.WriteLine("Data : " + received_data);
+        //string[] data = received_data.Split(',');
+        //Vector3 pose = new Vector3(Single.Parse(data[0]), Single.Parse(data[1]), Single.Parse(data[2]));
+        //Cam.transform.position = pose;
+        //writeSocket("Received");
+        /*switch (received_data)
+        {
+            case "pong":
+                Debug.Log("Python controller sent: " + (string)received_data);
+                writeSocket("ping");
+                break;
+            default:
+                Debug.Log("Nothing received");
+                break;
+        }*/
+    }
+    
+        void OnApplicationQuit()
+        {
+            closeSocket();
+        }
+
+        // Helper methods for:
+        //...setting up the communication
+        public void setupSocket()
+        {
+            try
+            {
+                tcp_socket = new TcpClient(host, port);
+                net_stream = tcp_socket.GetStream();
+                socket_writer = new StreamWriter(net_stream);
+                socket_reader = new StreamReader(net_stream);
+                socket_ready = true;
+            }
+            catch (Exception e)
+            {
+                // Something went wrong
+                Debug.Log("Socket error: " + e);
+            }
+        }
+
+        //... writing to a socket...
+        public void writeSocket(string line)
+        {
+            if (!socket_ready)
+                return;
+
+            
+            socket_writer.Write(line);
+            socket_writer.Flush();
+        }
+
+        //... reading from a socket...
+        public String readSocket()
+        {
+            if (socket_ready)
+                return "";
+
+        if (net_stream.DataAvailable)
+        {
+            return socket_reader.ReadLine();
+            
+        }
+            return "";
+            
+        }
+
+        //... closing a socket...
+        public void closeSocket()
+        {
+            if (!socket_ready)
+                return;
+
+            socket_writer.Close();
+            socket_reader.Close();
+            tcp_socket.Close();
+            socket_ready = false;
+        }
+    public static String Decode(string Path)
+    {
+        String text;
+        using (StreamReader sr = new StreamReader(Path))
+        {
+            text = sr.ReadToEnd();
+            byte[] bytes = Convert.FromBase64String(text);
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            System.Text.Decoder decoder = encoder.GetDecoder();
+            int count = decoder.GetCharCount(bytes, 0, bytes.Length);
+            char[] arr = new char[count];
+            decoder.GetChars(bytes, 0, bytes.Length, arr, 0);
+            text = new string(arr);
+
+            return text;
+        }
+    }
+}
+
+
+
+       
+
